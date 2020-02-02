ea4d6a68e87f8b8909b6611e434f5540655d538c
zhoua <870235784@qq.com>
2020-02-02 19:10:21 +0800

    添加kafka笔记

--- src/main/java/com/tca/kafka/producer/Producer.java
+++ src/main/java/com/tca/kafka/producer/Producer.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author zhoua
  * @Date 2019/11/6
@@ -17,12 +19,24 @@ public class Producer {
 
     private static final String SUCCESS = "SUCCESS";
 
+    private static final String TOPIC_NAME = "first-topic";
+
+    private static final String MESSAGE_PREFIX = "message-";
+
+    private static AtomicInteger count = new AtomicInteger();
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     @GetMapping("/{message}")
-    public String send(@PathVariable String message) {
-        kafkaTemplate.send("first-topic", message);
+    public String message(@PathVariable String message) {
+        kafkaTemplate.send("", message);
+        return SUCCESS;
+    }
+
+    @GetMapping("/send")
+    public String send() {
+        kafkaTemplate.send(TOPIC_NAME, MESSAGE_PREFIX + count.getAndIncrement());
         return SUCCESS;
     }
 
